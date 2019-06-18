b0de04b1aa7d28769907c1a9aa44c14ce4988dca
Yakushin103 <46379749+Yakushin103@users.noreply.github.com>
2019-06-18 23:31:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
   // высота 100%
 // $('.header').height($(window).height());
 //прокрутка до выброного в меню блока
- $(".navbar__link a").click(function(){
- 	$("body,html").animate({
- 		scrollTop:$("#" + $(this).data('value')).offset().top
- 	},1000)
+// $(".navbar__link a").click(function(){
+// 	$("body,html").animate({
+// 		scrollTop:$("#" + $(this).data('value')).offset().top
+// 	},1000)
+//
+//  })
 
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
   })
+
 })
